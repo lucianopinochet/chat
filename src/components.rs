c26e8b7cde71965b657c3f6521388895b4d3c838
Luciano <lucianobenjaminpm@gmail.com>
2023-08-28 10:46:24 -0400

    fixed stream sending

--- src/components.rs
+++ src/components.rs
@@ -1,16 +1,16 @@
 use std::{process::exit, time::Duration};
 use dioxus_router::prelude::*;
 use dioxus::prelude::*;
-use tokio::{net::TcpStream, sync::mpsc::Sender};
+use tokio::net::TcpStream;
 use crate::Route;
-// use futures_util::stream::StreamExt;
+use futures_util::stream::StreamExt;
 use tokio::io::{BufReader, AsyncReadExt, AsyncWriteExt};
 #[inline_props]
 pub fn Chat(cx: Scope) -> Element{
   let messages = use_ref(cx, || Vec::<String>::new());
   let messages_cloned = messages.clone();
-  let (tx, mut rx) = tokio::sync::mpsc::channel::<String>(1);
-  cx.spawn(async move{
+  // let (tx, mut rx) = tokio::sync::mpsc::channel::<String>(10);
+  let tx: &Coroutine<String> = use_coroutine(cx, |mut rx:UnboundedReceiver<String>| async move {
     let mut client = TcpStream::connect("localhost:3001").await.unwrap();
     let (reader, mut writer) = client.split();
     let mut reader = BufReader::new(reader);
@@ -26,46 +26,21 @@ pub fn Chat(cx: Scope) -> Element{
           };
           std::thread::sleep(Duration::from_millis(100));
         }
-        message = rx.recv() => {
-          println!("1: {message:?}");
-          writer.write_all(message.unwrap().as_bytes()).await.unwrap_or_else(|_| println!("message not sended"));
-          
+        message = rx.next() => {
+          match message {
+            Some(msg) => {
+              println!("1: {msg}");
+              writer.write_all(msg.to_string().as_bytes()).await.unwrap_or_else(|_| println!("message not sended"));
+              println!("2: {msg}");
+            },
+            None => {
+              println!("Error");
+            }
+          }
         }
       }      
     };
   });
-  // let tx: &Coroutine<String> = use_coroutine(cx, |mut rx:UnboundedReceiver<String>| async move {
-  //   let mut client = TcpStream::connect("localhost:3001").await.unwrap();
-  //   let (reader, mut writer) = client.split();
-  //   let mut reader = BufReader::new(reader);
-  //   let mut buffer = [0;64];
-  //   loop{
-  //     tokio::select! {
-  //       result = reader.read(&mut buffer) => {
-  //         messages_cloned.write().push(String::from_utf8_lossy(&buffer).to_string());
-  //         buffer = [0;64];
-  //         if let Err(_) = result{
-  //           println!("Connection Broked");
-  //           exit(0);
-  //         };
-  //         std::thread::sleep(Duration::from_millis(100));
-  //       }
-  //       message = rx.next() => {
-  //         match message {
-  //           Some(msg) => {
-  //             println!("1: {msg}");
-  //             writer.write_all(msg.to_string().as_bytes()).await.unwrap_or_else(|_| println!("message not sended"));
-  //             println!("2: {msg}");
-  //           },
-  //           None => {
-  //             println!("Error");
-  //           }
-  //         }
-  //       }
-  //     }      
-  //   };
-
-  // });
   let messages_lock = messages.read();
   let messages_rendered = messages_lock.iter().map(|message|{
     render!{
@@ -89,15 +64,13 @@ pub fn Chat(cx: Scope) -> Element{
   }
 }
 #[inline_props]
-fn SendBar<'a>(cx: Scope, messages: &'a UseRef<Vec<String>>, sender: Sender<String>) -> Element{
+fn SendBar<'a>(cx: Scope, messages: &'a UseRef<Vec<String>>, sender: &'a Coroutine<String>) -> Element{
   let message = use_state(cx, || "".to_string());
 	render!{
 			form{
 				onsubmit: move |_|{
 						messages.write().push(message.get().clone());
-            cx.spawn(async move{
-              sender.send(message.get().clone()).await;
-            });
+            sender.send(message.get().clone());
             println!("send");
 						message.set("".to_string())
 				},
@@ -154,3 +127,27 @@ pub fn Login(cx: Scope) -> Element{
           //     }
           //   }
           // }
+    // cx.spawn(async move{
+    //   let mut client = TcpStream::connect("localhost:3001").await.unwrap();
+    //   let (reader, mut writer) = client.split();
+    //   let mut reader = BufReader::new(reader);
+    //   let mut buffer = [0;64];
+    //   loop{
+    //     tokio::select! {
+    //       result = reader.read(&mut buffer) => {
+    //         messages_cloned.write().push(String::from_utf8_lossy(&buffer).to_string());
+    //         buffer = [0;64];
+    //         if let Err(_) = result{
+    //           println!("Connection Broked");
+    //           exit(0);
+    //         };
+    //         std::thread::sleep(Duration::from_millis(100));
+    //       }
+    //       message = rx.recv() => {
+    //         println!("1: {message:?}");
+    //         writer.write_all(message.unwrap().as_bytes()).await.unwrap_or_else(|_| println!("message not sended"));
+            
+    //       }
+    //     }      
+    //   };
+    // });
